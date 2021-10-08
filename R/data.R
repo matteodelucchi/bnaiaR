31459d7c81af2e1edfc15c0797a3bdc8f2123433
matteodelucchi <mail@matteodelucchi.com>
2021-10-08 16:00:43 +0200

    evidence knowledge graph

--- R/data.R
+++ R/data.R
@@ -131,3 +131,11 @@
 #'
 "exp11_dat"
 
+#' Evidence Knowledge Graph
+#'
+#' For details consider the vignette
+#' \code{vignette("evidenceknowledgegraph", package = "bnaiaR")}.
+#'
+#' @format A list of 3 items and class "bn".
+#'
+"ekg"
